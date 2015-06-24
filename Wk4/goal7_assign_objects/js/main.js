17a62c77218095370c21780619d5cdcad58e3851
Angel Valdes <AngelValdes@hotmail.com>
2015-06-24 14:27:51 -0400

    defining names array

--- Wk4/goal7_assign_objects/js/main.js
+++ Wk4/goal7_assign_objects/js/main.js
@@ -2,5 +2,13 @@
  * Created by the JavaScript Development Team
  * Class: PWA
  * Goal: Goal7
+ * Student Name: Angel Valdes
+ * Assignment : { Homework } - ANALYZE! OBJECTS.Person (4h)
+ * Date: 6/24/2015
+ * Note: PSEUDOCODE IN UPPERCASE
  */
+
+//ARRAY CALLED NAMES
+var names = ["Juan", "Jorge", "Mary", "Jose", "Cindy"];
+
 
--- Wk4/goal7_assign_objects/js/person.js
+++ Wk4/goal7_assign_objects/js/person.js
@@ -2,4 +2,10 @@
  * Created by the JavaScript Development Team
  * Class: PWA
  * Goal: Goal7
+ * Student Name: Angel Valdes
+ * Assignment : { Homework } - ANALYZE! OBJECTS.Person (4h)
+ * Date: 6/24/2015
+ * Note: PSEUDOCODE IN UPPERCASE
  */
+
+

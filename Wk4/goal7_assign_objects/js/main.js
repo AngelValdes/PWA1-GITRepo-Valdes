d739e5093f3158b287c4ad86f0c041fba6168bf2
Angel Valdes <AngelValdes@hotmail.com>
2015-06-24 14:29:33 -0400

    loop adding person objects to people array until I have three with unique names

--- Wk4/goal7_assign_objects/js/main.js
+++ Wk4/goal7_assign_objects/js/main.js
@@ -17,3 +17,12 @@ var people = [];
 //DECLACRE NAMES USED ARRAY
 var namesUsedIndexes = [];
 
+//LOOP UNTIL YOU HAVE THREE OBJECTS IN PEOPLE ARRAY, RANDOMLY CHOOSING A NAME FROM THE NAMES ARRAY, CHECKING IF ALREADY USED, THEN IF NOT USED, MAKE PERSON WITH THAT NAME USING "Person" TYPE AND ADD IT TO PEOPLE ARRAY, PASSING NAME AND ROW NUMBER FROM LOOP
+for (var i = 0; people.length < 3; i++) {
+    var nameIndex = Math.floor(Math.random() * 4);
+    if (findByProperty(namesUsedIndexes, nameIndex) == -1) {
+        var person = new Person(names[nameIndex], i);
+        people.push(person);
+        namesUsedIndexes.push(nameIndex);
+    }
+}
